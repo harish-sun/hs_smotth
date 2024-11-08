22587646d8492eb9c4148c0cc0ab9847f4d51756
harish-sun <harish.sun@gmail.com>
2024-11-08 13:04:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,6 @@ if ingredients_list:
         st.success('your smoothie is ordered ' + name_on_order)
         #st.write(my_insert_stmt)
 
-        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         #st.text(fruityvice_response.json())
-        # fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
